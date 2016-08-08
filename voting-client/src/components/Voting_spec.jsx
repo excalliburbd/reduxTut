024facce7d9d746403769519983306c38354fa36
excalliburbd <excalliburbd@gmail.com>
2016-08-08 22:21:41 +0600

    creat react app

---
+++ voting-client/src/components/Voting_spec.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithTag
+} from 'react-addons-test-utils';
+import Voting from '../../src/components/Voting';
+import { expect } from 'chai';
+
+describe('Voting', () => {
+  const components = renderIntoDocument(
+    <Voting pair={["Trainspotting", "28 Days Later"]} />
+  );
+  const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+  expect(buttons.length).to.equal(2);
+  expect(buttons[0].textContent).to.equal('Trainspotting');
+  expect(buttons[1].textContent).to.equal('28 Days Later');
+});
---
+++ voting-client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Voting from './components/Voting';
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={pair} />,
+  document.getElementById('root')
+);
